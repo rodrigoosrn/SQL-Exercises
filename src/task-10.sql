d87fba9a63cfdea5ee7fd8bea693d8b039a88127
rodrigoosrn <rodrigoosrn@gmail.com>
2017-09-27 11:21:40 -0300

    Update task-10.sql
    Added another solution for SQL Server.

--- src/task-10.sql
+++ src/task-10.sql
@@ -2,5 +2,10 @@
 Find the printer models having the highest price. Result set: model, price.
 */
 
-
+--solution 1
 SELECT model, price FROM printer WHERE price = ( SELECT MAX(price) FROM printer);
+
+--solution 2 for SQL Server
+SELECT TOP 1 WITH TIES model, price FROM printer
+ORDER BY price DESC
+
